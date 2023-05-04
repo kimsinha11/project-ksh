87266e651c4e37527b5e0a3ff3482231543d4403
kimsinha11 <tlsgk2013@gmail.com>
2023-05-05 00:12:11 +0900

    회원가입 구현, Ut.empty함수 도입/구현

--- src/main/java/com/KoreaIT/ksh/demo/controller/UsrMemberController.java
+++ src/main/java/com/KoreaIT/ksh/demo/controller/UsrMemberController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.KoreaIT.ksh.demo.service.MemberService;
+import com.KoreaIT.ksh.demo.util.Ut;
+import com.KoreaIT.ksh.demo.vo.Member;
 
 @Controller
 public class UsrMemberController {
@@ -14,11 +16,37 @@ public class UsrMemberController {
 
 	@RequestMapping("/usr/member/doJoin")
 	@ResponseBody
-	public String doJoin(String loginId, String loginPw, String name, String nickname, String cellphoneNum,
+	public Object doJoin(String loginId, String loginPw, String name, String nickname, String cellphoneNum,
 			String email) {
-
-		memberService.doJoin(loginId, loginPw, name, nickname, cellphoneNum, email);
-
-		return "가입!";
+		
+		if (Ut.empty(loginId)) {
+			return "아이디를 입력해주세요";
+		}
+		if (Ut.empty(loginPw)) {
+			return "비밀번호를 입력해주세요";
+		}
+		if (Ut.empty(name)) {
+			return "이름을 입력해주세요";
+		}
+		if (Ut.empty(nickname)) {
+			return "닉네임을 입력해주세요";
+		}
+		if (Ut.empty(cellphoneNum)) {
+			return "전화번호를 입력해주세요";
+		}
+		if (Ut.empty(email)) {
+			return "이메일을 입력해주세요";
+		}
+		
+		int id = memberService.doJoin(loginId, loginPw, name, nickname, cellphoneNum, email);
+		if(id == -1) {
+			return "이미 사용중인 아이디입니다";
+		}
+		if(id == -2) {
+			return "이미 사용중인 이름과 이메일입니다";
+		}
+		Member member = memberService.getMemberById(id);
+		
+		return member;
 	}
 }
--- src/main/java/com/KoreaIT/ksh/demo/repository/MemberRepository.java
+++ src/main/java/com/KoreaIT/ksh/demo/repository/MemberRepository.java
@@ -2,10 +2,20 @@ package com.KoreaIT.ksh.demo.repository;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import com.KoreaIT.ksh.demo.vo.Member;
+
 @Mapper
 public interface MemberRepository {
 
 	// 서비스 메서드
 	void doJoin(String loginId, String loginPw, String name, String nickname, String cellphoneNum, String email);
+
+	int getLastInsertId();
+
+	Member getMemberById(int id);
+
+	Member getMemberByLoginId(String loginId);
+
+	Member getMemberByNameAndEmail(String name, String email);
 			
 }
