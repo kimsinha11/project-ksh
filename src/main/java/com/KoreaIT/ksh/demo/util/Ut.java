d6108d0f2d5bdd217c037106df256d75cc24d418
kimsinha11 <tlsgk2013@gmail.com>
2023-05-05 00:16:25 +0900

    Ut.f 함수도입, MemberController에 적용

--- src/main/java/com/KoreaIT/ksh/demo/util/Ut.java
+++ src/main/java/com/KoreaIT/ksh/demo/util/Ut.java
@@ -12,4 +12,8 @@ public class Ut {
 		String str = (String) obj;
 		return str.trim().length() == 0;
 	}
+	
+	public static String f(String format, Object... args) {
+		return String.format(format, args);
+	}
 }
