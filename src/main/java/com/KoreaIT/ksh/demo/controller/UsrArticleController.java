77c1c0a22b3538c2e94e5537304caab15e7e7a1d
kimsinha11 <tlsgk2013@gmail.com>
2023-05-05 01:14:58 +0900

    session활용해 로그인/로그아웃 구현

--- src/main/java/com/KoreaIT/ksh/demo/controller/UsrArticleController.java
+++ src/main/java/com/KoreaIT/ksh/demo/controller/UsrArticleController.java
@@ -1,7 +1,5 @@
 package com.KoreaIT.ksh.demo.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,13 +18,20 @@ public class UsrArticleController {
 	@RequestMapping("/usr/article/doWrite")
 	@ResponseBody
 	public ResultData doWrite(String title, String body) {
-		if(Ut.empty(title)) {
-			return ResultData.from("F-1", Ut.f("제목을 입력해주세요"));
+		if (Ut.empty(title)) {
+			return ResultData.from("F-1", "제목을 입력해주세요");
 		}
+		if (Ut.empty(body)) {
+			return ResultData.from("F-2", "내용을 입력해주세요");
+		}
+
 		ResultData writeArticleRd = articleService.writeArticle(title, body);
+
 		int id = (int) writeArticleRd.getData1();
+
 		Article article = articleService.getArticle(id);
-		return ResultData.from(writeArticleRd.getResultCode(), writeArticleRd.getMsg(), article);
+
+		return ResultData.newData(writeArticleRd, article);
 	}
 	
 
--- src/main/java/com/KoreaIT/ksh/demo/controller/UsrMemberController.java
+++ src/main/java/com/KoreaIT/ksh/demo/controller/UsrMemberController.java
@@ -1,5 +1,7 @@
 package com.KoreaIT.ksh.demo.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -44,8 +46,63 @@ public class UsrMemberController {
 			return joinRd;
 		}
 
+		
+		
 		Member member = memberService.getMemberById((int) joinRd.getData1());
 
 		return ResultData.newData(joinRd, member);
+	}
+	
+	@RequestMapping("/usr/member/doLogin")
+	@ResponseBody
+	public ResultData doLogin(HttpSession httpSession, String loginId, String loginPw) {
+		boolean isLogined = false;
+		
+		if(httpSession.getAttribute("loginedMemberId")!=null) {
+			isLogined = true;
+		}
+		//F-A 로그인 오류 F-B 로그아웃 오류 F-N 빈 값 오류 F-F 없거나 불일치 오류
+		if(isLogined) {
+			return ResultData.from("F-A", "이미 로그인 상태입니다");
+		}
+		if(Ut.empty(loginId)) {
+			return ResultData.from("F-N", "아이디를 입력해주세요");
+		}
+		if(Ut.empty(loginPw)) {
+			return ResultData.from("F-N", "비밀번호를 입력해주세요");
+		}
+		
+		Member member = memberService.getMemberByLoginId(loginId);
+		
+		if (member == null) {
+			return ResultData.from("F-F", Ut.f("%s는 존재하지 않는 아이디입니다", loginId));
+		}
+
+		if (member.getLoginPw().equals(loginPw) == false) {
+			return ResultData.from("F-F", Ut.f("비밀번호가 일치하지 않습니다"));
+		}
+		
+		httpSession.setAttribute("loginedMemberId", member.getId());
+		
+		return ResultData.from("S-1", Ut.f("%s님 환영합니다", member.getNickname()));
+	}
+	
+	@RequestMapping("/usr/member/doLogout")
+	@ResponseBody
+	public ResultData doLogout(HttpSession httpSession) {
+		boolean isLogined = false;
+		
+		if(httpSession.getAttribute("loginedMEmberid")==null) {
+		
+			isLogined = true;
+		}
+		
+		if(isLogined) {
+			return ResultData.from("F-B", "이미 로그아웃 상태입니다");
+		}
+		
+		httpSession.removeAttribute("loginedMemberId");	
+		
+		return ResultData.from("S-1", "로그아웃 되었습니다");
 	}
 }
