d8f5eb7cfe2280d79ec7e305b51275c4c3e46cc3
kimsinha11 <tlsgk2013@gmail.com>
2023-05-13 23:20:27 +0900

    날씨 API 데이터 적용은 성공, 좀 더 구현해야함

--- src/main/java/com/KoreaIT/ksh/demo/controller/WeatherApiController.java
+++ src/main/java/com/KoreaIT/ksh/demo/controller/WeatherApiController.java
@@ -49,7 +49,15 @@ public class WeatherApiController {
 
 		model.addAttribute("result", resultMap);
 
+		String responseBody = (String) resultMap.get("responseBody");
+		if (responseBody != null) {
+			model.addAttribute("responseBody", responseBody);
+		} else {
+			model.addAttribute("responseBody", ""); // responseBody가 null일 경우 빈 문자열을 넘겨줌
+		}
+
 		return "/usr/home/APITest2";
+
 	}
 
 	public HashMap<String, Object> getDataFromJson(String url, String encoding, String type, String jsonStr)
@@ -63,44 +71,48 @@ public class WeatherApiController {
 		}
 
 		return getStringFromURL(url, encoding, isPost, jsonStr, "application/json");
-	}public HashMap<String, Object> getStringFromURL(String url, String encoding, boolean isPost, String parameter, String contentType) throws Exception {
-        URL apiURL = new URL(url);
-        HttpURLConnection conn = (HttpURLConnection) apiURL.openConnection();
-        conn.setConnectTimeout(5000);
-        conn.setReadTimeout(5000);
-        conn.setUseCaches(false);
-        conn.setRequestProperty("Content-Type", contentType);
-        conn.setRequestProperty("Accept-Charset", encoding);
-        
-        if (isPost) {
-            conn.setRequestMethod("POST");
-            conn.setDoOutput(true);
-            OutputStreamWriter osw = new OutputStreamWriter(conn.getOutputStream(), encoding);
-            osw.write(parameter);
-            osw.flush();
-            osw.close();
-        }
-        
-        int responseCode = conn.getResponseCode();
-        BufferedReader in;
-        if (responseCode == HttpURLConnection.HTTP_OK) {
-            in = new BufferedReader(new InputStreamReader(conn.getInputStream(), encoding));
-        } else {
-            in = new BufferedReader(new InputStreamReader(conn.getErrorStream(), encoding));
-        }
-        
-        String inputLine;
-        StringBuilder response = new StringBuilder();
-        while ((inputLine = in.readLine()) != null) {
-            response.append(inputLine);
-        }
-        in.close();
-        
-        HashMap<String, Object> resultMap = new HashMap<>();
-        resultMap.put("responseCode", responseCode);
-        resultMap.put("responseMessage", conn.getResponseMessage());
-        resultMap.put("responseBody", response.toString());
-        
-        return resultMap;
-    }
+	}
+
+	public HashMap<String, Object> getStringFromURL(String url, String encoding, boolean isPost, String parameter,
+			String contentType) throws Exception {
+		URL apiURL = new URL(url);
+		HttpURLConnection conn = (HttpURLConnection) apiURL.openConnection();
+		conn.setConnectTimeout(5000);
+		conn.setReadTimeout(5000);
+		conn.setUseCaches(false);
+		conn.setRequestProperty("Content-Type", contentType);
+		conn.setRequestProperty("Accept-Charset", encoding);
+
+		if (isPost) {
+			conn.setRequestMethod("POST");
+			conn.setDoOutput(true);
+			OutputStreamWriter osw = new OutputStreamWriter(conn.getOutputStream(), encoding);
+			osw.write(parameter);
+			osw.flush();
+			osw.close();
+		}
+
+		int responseCode = conn.getResponseCode();
+		BufferedReader in;
+		if (responseCode == HttpURLConnection.HTTP_OK) {
+			in = new BufferedReader(new InputStreamReader(conn.getInputStream(), encoding));
+		} else {
+			in = new BufferedReader(new InputStreamReader(conn.getErrorStream(), encoding));
+		}
+
+		String inputLine;
+		StringBuilder response = new StringBuilder();
+		while ((inputLine = in.readLine()) != null) {
+			response.append(inputLine);
+		}
+		in.close();
+
+		HashMap<String, Object> resultMap = new HashMap<>();
+		resultMap.put("responseCode", responseCode);
+		resultMap.put("responseMessage", conn.getResponseMessage());
+		resultMap.put("responseBody", response.toString());
+
+		return resultMap;
+	}
+
 }
